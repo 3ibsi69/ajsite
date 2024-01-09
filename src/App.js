2f5de22191668d75ad13b9509f35700dddfdf8e4
3ibsi69 <a.abbassi21306@pi.tn>
2024-01-09 12:49:06 +0100

    end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Index from './index.jsx'; // Fix the casing of the file name
+import Index from './index.jsx'; 
 import JeuxpedComponent from './Types/Jeuxped';
 function App() {
   return (
     <div className="App">
       <Index/>
+      {/* <JeuxpedComponent/> */}
     </div>
   );
 }
--- src/Types/Jeuxped.jsx
+++ src/Types/Jeuxped.jsx
@@ -21,8 +21,38 @@ const JeuxpedComponent = () => {
       <p id="textintro">
           Introduire du jeu dans la pédagogie, c'est vouloir mêler plaisir et
           travail.
-        
       </p>
+      <p id='charade'>Charades</p>
+      <div className="last-text">
+      <p>Ce jeu est un moyen d'encourager a participer a la lecon</p>
+      {/* make list a b c  */}
+      <ol type='a'>
+        <li>
+          <p>Ressource: une liste de personnes ,d'action </p>
+        </li>
+        <li>
+          <p>Jeu : divisiz votre classe en deux equipes puis pensez a un mot lie a la lecon </p>
+        </li>
+        <li>
+          <p>Les differnte type de charrade</p>
+          <ul>
+            <li>
+              <p>la Charade classique</p>
+            </li>
+            <li>
+              <p>Charade  repetitive</p>
+            </li>
+            <li>
+              <p>Charade a tiroirs</p>
+            </li>
+
+          </ul>
+        </li>
+
+      </ol>
+
+
+      </div>
     </div>
   );
 };
